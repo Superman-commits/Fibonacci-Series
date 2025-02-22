297fc2508a065001bf2857be21019f143c511cea
Superman-commits <vgarun172@gmail.com>
2025-02-22 21:48:13 +0530

    feat: Added features for fibonacci series generator in C# using .NET framework

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int a=0;
+int b=1;
+
+Console.WriteLine("Enter the number of terms in the Fibonacci Series: ");
+string number=Console.ReadLine()!;
+//the purpose of the ! symbol in the above line ending signifies that the number input won't be null
+int n=int.Parse(number);
+
+if(n>0)
+{
+    Console.WriteLine("The Fibonacci Series is as follows: ");
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    
+    for(int i=2;i<n;i++)
+    {
+        int c=a+b;
+        Console.WriteLine(c);
+        a=b;
+        b=c;
+    }
+}
+else
+{
+    Console.WriteLine("The number of terms entered is not proper. Try again!!!!");
+}
---
+++ Fibonacci/README.md
@@ -0,0 +1,5 @@
+# Fibonacci Series Generator
+- ** The goal is to find the fibonacci series upto the number of terms mentioned **
+- ** Prompts the user for input **
+- ** Generates the desired output if input is valid **
+- ** Handles invalid input as well, just in case **
